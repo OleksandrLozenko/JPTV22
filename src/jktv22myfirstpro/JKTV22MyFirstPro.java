5629ae24a06ef4d596c9e7dd71411cebd466e42d
Oleksandr Lozenko <131644673+OleksandrLozenko@users.noreply.github.com>
2023-09-14 12:42:34 +0300

    Update JKTV22MyFirstPro.java

--- src/jktv22myfirstpro/JKTV22MyFirstPro.java
+++ src/jktv22myfirstpro/JKTV22MyFirstPro.java
@@ -7,14 +7,7 @@ public class JKTV22MyFirstPro {
         String firstname = scanner.nextLine();
         System.out.print("Введите вашу фамилию: ");
         String lastname = scanner.nextLine();
-        System.out.println("Hello " + firstname + " " + lastname + "!");
-        System.out.print("Введите год вашего рождения: ");
-        int currentYear = 2023;
-        int currentMonth = 9;
-        int currentDay = 1;
-        int yourYear = scanner.nextInt();
-        int old = currentYear - yourYear;
-        System.out.println("Вам " + old + " лет");
+        System.out.println("Привет " + firstname + " " + lastname + "!");
     }
     
 }
